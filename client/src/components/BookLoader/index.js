0ec62b26fabe4383880df15b0cf0083cdd1c1772
Ricardo Vera <ricardoveramedina@gmail.com>
2020-09-27 01:20:05 +0900

    react added and BookLoader component

---
+++ client/src/components/BookLoader/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './style.scss';
+
+//https://dribbble.com/shots/7199149-Book-Loader
+const BookLoader = () => {
+  return (
+    <div className="book">
+      <div className="inner">
+        <div className="left" />
+        <div className="middle" />
+        <div className="right" />
+      </div>
+      <ul>
+        <li />
+        <li />
+        <li />
+        <li />
+        <li />
+        <li />
+        <li />
+        <li />
+        <li />
+        <li />
+        <li />
+        <li />
+        <li />
+        <li />
+        <li />
+        <li />
+        <li />
+        <li />
+      </ul>
+    </div>
+  );
+};
+
+export { BookLoader };
