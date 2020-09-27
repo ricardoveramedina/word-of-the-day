b3db98f4e4e454289085b0ddbe40a217f0d81733
Ricardo Vera <ricardoveramedina@gmail.com>
2020-09-27 20:47:38 +0900

    cors added

--- server/server.js
+++ server/server.js
@@ -2,23 +2,25 @@
 const config = require('config');
 const fetcher = require('./fetcher');
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 
 const appConfig = {};
 const scrapperConfig = {};
 
 if (config.has('appConfig')) {
-    appConfig.port = config.appConfig.port;
-    scrapperConfig.url = config.scraperConfig.url;
-    scrapperConfig.totalPages = config.scraperConfig.totalPages;
+  appConfig.port = config.appConfig.port;
+  scrapperConfig.url = config.scraperConfig.url;
+  scrapperConfig.totalPages = config.scraperConfig.totalPages;
 } else {
-    throw new Error('no config file');
+  throw new Error('no config file');
 }
 
 app.get('/randomWord', (req, res) => {
-    fetcher
-        .fetchWord(scrapperConfig.url, scrapperConfig.totalPages)
-        .then((data) => res.send(data));
+  fetcher
+    .fetchWord(scrapperConfig.url, scrapperConfig.totalPages)
+    .then((data) => res.send(data));
 });
 
 app.listen(appConfig.port, () => {});
